d3c2d9bfc04e3b28087cb6928bc972f91d6e9704
Drew Jocham <drew.jocham@gmail.com>
2022-11-10 21:05:14 +0100

    add grpc client

--- form/vue.config.js
+++ form/vue.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin')
-
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = defineConfig({
-  transpileDependencies: true,
-    chainWebpack: config => {
-        config.plugin('monaco-editor').use(MonacoWebpackPlugin, [
-            {
-                // Languages are loaded on demand at runtime
-                languages: ['json', 'javascript', 'html', 'xml', 'python', 'java', 'go']
-            }
-        ])
+    transpileDependencies: true,
+    configureWebpack: {
+        plugins: [
+            new NodePolyfillPlugin()
+        ]
     }
 })
